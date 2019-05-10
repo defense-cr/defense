fdf78b844872569a3eacdba25855e335655d5197
Florin Lipan <florinlipan@gmail.com>
2019-05-10 15:19:07 +0200

    Introduce DEFAULT_THROTTLED_RESPONSE

--- src/defense/handler.cr
+++ src/defense/handler.cr
@@ -4,14 +4,18 @@ module Defense
   class Handler
     include HTTP::Handler
 
+    DEFAULT_THROTTLED_RESPONSE = ->(response : HTTP::Server::Response) do
+      response.status = HTTP::Status::TOO_MANY_REQUESTS
+      response.content_type = "text/plain"
+      response.puts("Retry later\n")
+    end
+
     def initialize
     end
 
     def call(ctx : HTTP::Server::Context)
       if Defense.throttled?(ctx.request, ctx.response)
-        ctx.response.status = HTTP::Status::TOO_MANY_REQUESTS
-        ctx.response.content_type = "text/plain"
-        ctx.response.puts("Retry later\n")
+        DEFAULT_THROTTLED_RESPONSE.call(ctx.response)
       else
         call_next(ctx)
       end
